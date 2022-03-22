d86cc737711514cc39aedba469418aab18695ed4
Jhoandry Sequera <jsequera@optoro.com>
2022-03-21 23:53:14 -0300

    DT: first commit

---
+++ src/main/java/com/agenda/dailyTask/DailyTask/DailyTaskApplication.java
@@ -0,0 +1,13 @@
+package com.agenda.dailyTask.DailyTask;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DailyTaskApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DailyTaskApplication.class, args);
+	}
+
+}
---
+++ src/main/java/com/agenda/dailyTask/DailyTask/ServletInitializer.java
@@ -0,0 +1,13 @@
+package com.agenda.dailyTask.DailyTask;
+
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+
+public class ServletInitializer extends SpringBootServletInitializer {
+
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+		return application.sources(DailyTaskApplication.class);
+	}
+
+}
